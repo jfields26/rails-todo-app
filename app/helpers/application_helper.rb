1a07dd9e5db1a0f3e01048dcb936dabfea92184b
Jarred Fields <jrfields26@icloud.com>
2026-05-04 00:42:57 -0400

    Fix return_to navigation for todos show/edit flow

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,3 @@
 # Jarred Fields
-
 module ApplicationHelper
-  def back_path_from_origin(todo, origin)
-    case origin
-    when "completed"
-      completed_todos_path
-    when "category"
-      category_path(todo.category)
-    else
-      todos_path
-    end
-  end
 end
